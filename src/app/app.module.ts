9c62f0396e387f377d351c02b6f138dc68f9970f
Sven Leonhardt <sven.leonhardt@sovanta.com>
2018-11-03 17:34:49 +0100

    add header and first climate panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MatCardModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClimatePanelComponent } from './climate-panel/climate-panel.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, HeaderComponent, ClimatePanelComponent],
+    imports: [BrowserModule, AppRoutingModule, MatCardModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
---
+++ src/app/climate-panel/climate-panel.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-climate-panel',
+    templateUrl: './climate-panel.component.html',
+    styleUrls: ['./climate-panel.component.scss']
+})
+export class ClimatePanelComponent implements OnInit {
+    @Input()
+    public indoor = true;
+
+    @Input()
+    set temp(value: number) {
+        this.integerPart = Math.floor(value);
+        this.decimalPart = (value - this.integerPart).toFixed(1).substring(2);
+    }
+    public integerPart = 0;
+    public decimalPart = 0;
+
+    @Input()
+    public high = 0;
+
+    constructor() {}
+
+    ngOnInit() {}
+}
